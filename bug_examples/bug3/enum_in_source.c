f5820ec05a4680ab9fc2f948c99a1237b95d0206
Paul <paul@taniustech.com>
2017-09-28 13:24:51 -0700

    bug3

---
+++ bug_examples/bug3/enum_in_source.c
@@ -0,0 +1,18 @@
+
+#include "enum_in_source.h"
+
+#if INTERFACE
+
+enum TestEnum
+{
+    EnumVal1,
+    EnumVal2,
+};
+
+struct S1
+{
+    int e;
+};
+
+
+#endif
---
+++ bug_examples/bug3/try_to_use_struct.c
@@ -0,0 +1,13 @@
+
+#include "try_to_use_struct.h"
+
+#if INTERFACE
+#endif
+
+
+static S1 f()
+{
+    S1 s;
+    s.e = EnumVal1;
+    return s;
+}
